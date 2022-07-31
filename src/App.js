116c77fe7926a2dcd2529e279636059ce5546aaa
Danijel-Enoch <enochdanijel@gmail.com>
2022-07-31 02:50:45 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ethers } from "ethers";
+import abi from ".//abi.json"
+
+async function  connectToMetamask() {
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const accounts = await provider.send("eth_requestAccounts", []);
+  this.setState({ selectedAddress: accounts[0] })
+}
+const multi_send=async(addresses,amount)=>{
+  try{
+    const Addresses_array=addresses.split(",");
+    const amount_list_array=amount.split(",");
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const address = "0xeA90ccC65D86Cb2549567368C1d4841FFd1B89DC";
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(address, abi, signer);   
+    const tx = await contract.functions._multiSendToken(address,Addresses_array,amount_list_array);
+    const receipt = await tx.wait();
+    console.log("receipt", receipt);
+    alert("sent")
+  }catch(error){
+    console.log(error)
+  }
+ 
+}
 
 function App() {
+  const [addresses, set_addressess] = useState("");
+  const [amount_list,set_amount_list]=useState();
+  
+  const handleSubmit = (event) => {
+    multi_send(addresses,amount_list)
+    // event.preventDefault();
+    // alert(`The name you entered was: ${amount_list}`)
+  }
+
+  
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <label>Enter List of Wallet Address:
+        <input
+          type="text" 
+          value={addresses}
+          onChange={(e) => set_addressess(e.target.value)}
+        />
+      </label>
+      <label>Enter List of amount:
+        <input
+          type="text" 
+          value={amount_list}
+          onChange={(e) => set_amount_list(e.target.value)}
+        />
+      </label>
+      <input type="button"  value={"sendd"} onClick={handleSubmit} />
+      <input type="button" value={"connect"} placeholder='Connect Wallet ' onClick={connectToMetamask} />
+    </form>
   );
 }
 
